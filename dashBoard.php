e932c099cd1e89f7cc4c737ef52a006ac9966335
RaresCOD <raresandreicodreanu@yahoo.com>
2021-08-04 11:13:19 +0300

    Initial commit

---
+++ dashBoard.php
@@ -0,0 +1,162 @@
+<?php
+  session_start();
+  ob_start();
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="custom.css">
+    <link hrepf="buttons.css" rel="stylesheet">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+    <div class="container">
+      <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+
+        </ul>
+
+        <div class="col-md-3 text-end">
+          <?php
+            if(!isset($_SESSION)) {
+              $class = 'show';
+            } else {
+              $class = 'hidden';
+            }
+           ?>
+          <a href="login.php"><button type="button" class="btn btn-outline-primary me-2, <?= $class?>">Login</button></a>
+          <a href="register.php"><button type="button" class="btn btn-primary">Sign-up</button></a>
+          <a href="logout.php"><button type="button" class="btn btn-primary">Logout</button></a>
+        </div>
+      </header>
+    </div>
+    <form action="" method="post">
+
+      <div class="row g-2">
+        <div class="col-5">
+          <div class="form-floating">
+            <input type="text" name="task" class="form-control" id="floatingInputGrid" placeholder="Mers la magazin">
+            <label for="floatingInputGrid">Task</label>
+          </div>
+        </div>
+        <div class="col-5">
+          <div class="form-floating">
+            <select class="form-select" name="importance" id="floatingSelectGrid" aria-label="Floating label select example">
+              <option selected>Open this select menu</option>
+              <option value="1">Low</option>
+              <option value="2">Medium</option>
+              <option value="3">High</option>
+            </select>
+            <label for="floatingSelectGrid">Importance</label>
+          </div>
+        </div>
+        <div class="col m-2"><button class="w-100 btn btn-lg btn-primary" name="add" type="submit">Add</button></div>
+      </div>
+    </form>
+    <?php
+
+
+      //try {
+        $pdo = new PDO('sqlite:TASKS.db');
+
+        $pdo->exec("CREATE TABLE TODOS(id INTEGER PRIMARY KEY,userid INTEGER,task TEXT, importance INTEGER);");
+      //} catch (PDOException $e) {
+      //  echo $e->getMessage();
+    //  }
+
+      // function deleteTask($taskId) {
+      //   $sql = 'DELETE FROM TODOS '.'WHERE id = :id';
+      //   $stmt = $pdo->exec($sql);
+      //   $stmt->bindValue(':id', $taskId);
+      //   $stmt->execute();
+      //   return $stmt->rowcount();
+      // }
+      ?>
+      <br>
+
+      <?php
+        $userid = $_SESSION["id"];
+        if (gettype($userid) == "string") {
+          $userid = intval($userid);
+        }
+        $email = $_SESSION['email'];
+
+        if (isset($_POST['add'])) {
+          $task = $_POST['task'];
+          $importance = $_POST['importance'];
+          $id = intval (uniqid (rand (),true));
+          echo $id. " = ". gettype($id) . "<br>" . $userid. " = " . gettype($userid). "<br>" . $task. " = " . gettype($task). " " . $importance. " = ". gettype($importance)."<br>";
+          //$output = ob_get_contents();
+          //echo "<br>".$output;
+          echo "da";
+          $pdo->exec("INSERT INTO TODOS(id,userid,task,importance) VALUES('$id','$userid','$task','$importance');");
+          $string = ("INSERT INTO TODOS(id,userid,task,importance) VALUES('$id','$userid','$task','$importance');");
+          echo $string;
+          //die();
+          //echo $da;
+          //$pdo->exec("INSERT INTO TODOS(id,userid,task,importance) VALUES('$id','$userid','dada','mportancae');");
+          unset($_POST['add']);
+        }
+        //session_destroy();\
+        $result = $pdo->query("SELECT * FROM TODOS");
+       ?>
+
+      <h2>List of tasks:<br></h2>
+      <ul class="list-group list-group-flush">
+        <?php foreach ($result as $row): ?>
+          <form action="" method="post">
+            <div class="row g-2">
+              <?php
+                if ($row['importance'] == '3') {
+                  ?><div class="col-8"><li class="list-group-item bg-danger"><?= $row['task'] ?></li></div><?php
+                } elseif ($row['importance'] == '2') {
+                  ?><div class="col-8"><li class="list-group-item bg-warning"><?= $row['task'] ?></li></div><?php
+                } else {
+                  ?><div class="col-8"><li class="list-group-item bg-succes"><?= $row['task'] ?></li></div><?php
+                }
+              ?>
+
+              <?php
+
+
+                if(isset($_REQUEST['del'])) {
+                  //deleteTask($row['id']);
+                  unset($_REQUEST['del']);
+                }
+                if(isset($_REQUEST['edit'])) {
+                  echo "Edit";
+                  unset($_REQUEST['edit']);
+                }
+                if(isset($_REQUEST['com'])) {
+                  echo "Complited";
+                  unset($_REQUEST['com']);
+                }
+              ?>
+
+                <div class="col m-2"><input type="submit" name="del" class="w-100 btn btn-lg btn-primary" value="Delete"></div>
+                <div class="col m-2"><input type="submit" name="edit" class="w-100 btn btn-lg btn-primary" value="Edit"></div>
+                <div class="col m-2"><input type="submit" name="com" class="w-100 btn btn-lg btn-primary" value="Complited"></div>
+                <br>
+
+            </div>
+          </form>
+        <?php endforeach; ?>
+      </ul>
+
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,103 @@
+<html lang="en">
+
+<?php
+  include_once 'register_header.php';
+  ob_start();
+  session_start();
+  //include_once 'users.db';
+ ?>
+
+<body class="text-center">
+
+<main class="form-signin">
+  <form action="" method="post">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <img class="mb-4" onerror="this.onerror=null" src="user-icon-trendy-flat-style-isolated-grey-background-user-symbol-user-icon-trendy-flat-style-isolated-grey-background-123663211.jpeg" alt="" width="72" height="72">
+    <h1 class="h3 mb-3 fw-normal">Please Login</h1>
+
+    <div class="form-floating">
+      <input type="email" name="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">Email address</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword">Password</label>
+    </div>
+
+    <div class="checkbox mb-3">
+      <label>
+        <input type="checkbox" value="remember-me"> Remember me
+      </label>
+      <br>
+      <a href="register.php">Don't have an account?</a>
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
+
+  </form>
+<?php
+
+  try {
+    $pdo = new PDO('sqlite:users.db');
+
+    $pdo->exec("CREATE TABLE groups(id INTEGER PRIMARY KEY,firstName TEXT,lastName TEXT,email TEXT,password TEXT)");
+
+
+    //$statement = $pdo->query("SELECT * FROM users");
+    //rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+    //print_r($rows);
+  } catch (PDOException $e) {
+    echo $e->getMessage();
+  }
+
+
+  function valid($email, $password, $pdo) {
+    $validUser = false;
+    $result = $pdo->query("SELECT * FROM groups");
+    foreach ($result as $row) {
+      if ($email == $row['email'] and $password == $row['password']) {
+        $validUser = true;
+      }
+    }
+    return $validUser;
+  }
+
+  function getId($email, $password, $pdo) {
+    $id = null;
+    $result = $pdo->query("SELECT * FROM groups");
+    foreach ($result as $row) {
+      if ($email == $row['email'] and $password == $row['password']) {
+        $id = $row['id'];
+        return $id;
+      }
+    }
+    return $id;
+  }
+
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    if ($email == null || $password == null) {
+      echo "All fileds must be complited";
+    } else {
+      if (!valid($email, $password, $pdo)) { //validam datele introduse de user
+
+      } else {
+        echo "Login succesful";
+        $_SESSION["id"] = getId($email, $password, $pdo);
+        $_SESSION["email"] = $email;
+        header('Location: dashBoard.php');
+        ob_end_flush();
+      }
+    }
+  }
+ ?>
+
+</main>
+
+
+
+
+
+</body></html>
